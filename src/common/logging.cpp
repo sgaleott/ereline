1a7cc05c014bc74dcb71d8d7850023608d425d8c
Maurizio Tomasi <ziotom78@gmail.com>
2013-12-04 17:28:14 +0100

    New class for logging
    The Logging class should be used for logging useful information (error, warnings, informative messages and debug strings). It supports arbitrary nesting levels of indentation, multiple message types, and the log messages can be sent to any output stream (a string, a file, or the terminal -- the latter is the default).

---
+++ src/common/logging.cpp
@@ -0,0 +1,56 @@
+#include "logging.hpp"
+#include <iostream>
+
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/date_time/posix_time/ptime.hpp>
+#include <boost/format.hpp>
+
+bool Logger::exist_instance_flag = false;
+Logger * Logger::singleton = nullptr;
+
+////////////////////////////////////////////////////////////////////////
+
+Logger * 
+Logger::get_instance()
+{
+    if(! exist_instance_flag) {
+	singleton = new Logger();
+	exist_instance_flag = true;
+    }
+
+    return singleton;
+}
+
+////////////////////////////////////////////////////////////////////////
+
+static std::string
+current_date_and_time()
+{
+    boost::posix_time::ptime now(boost::date_time::second_clock<boost::posix_time::ptime>::universal_time());
+
+    return boost::posix_time::to_iso_extended_string(now);
+}
+
+////////////////////////////////////////////////////////////////////////
+
+void 
+Logger::log(Log_level level, const std::string & string) const
+{
+    if(level < log_level || log_stream == nullptr)
+	return; // Do not log anything
+
+    std::string error_type;
+    switch(level) {
+    case Log_level::DEBUG:   error_type = "  DEBUG"; break;
+    case Log_level::INFO:    error_type = "   INFO"; break;
+    case Log_level::WARNING: error_type = "WARNING"; break;
+    case Log_level::ERROR:   error_type = "  ERROR"; break;
+    }
+
+    (*log_stream) << (boost::format("%1% %2%: %3%%4%") 
+		      % current_date_and_time() 
+		      % error_type 
+		      % std::string(indent_level, '\t') // Indent
+		      % string)
+		  << std::endl;
+}
---
+++ src/common/logging.hpp
@@ -0,0 +1,52 @@
+#ifndef LOGGING_HPP
+#define LOGGING_HPP
+
+#include <string>
+#include <ostream>
+#include <boost/format.hpp>
+
+class Logger {
+public:
+    enum class Log_level { DEBUG, INFO, WARNING, ERROR };
+
+    static Logger * get_instance();
+
+    void log(Log_level level, const std::string & string) const;
+
+    void increase_indent() { indent_level++; }
+    void decrease_indent() { if(indent_level > 0) indent_level--; }
+
+    void set_log_level(Log_level new_level);
+
+    void set_stream(std::ostream * new_log_stream) { log_stream = new_log_stream; }
+
+    void debug(const std::string & string) const { log(Log_level::DEBUG, string); }
+    void debug(const boost::format & string_fmt) const { debug(boost::str(string_fmt)); }
+
+    void info(const std::string & string) const { log(Log_level::INFO, string); }
+    void info(const boost::format & string_fmt) const { info(boost::str(string_fmt)); }
+
+    void warning(const std::string & string) const { log(Log_level::WARNING, string); }
+    void warning(const boost::format & string_fmt) const { warning(boost::str(string_fmt)); }
+
+    void error(const std::string & string) const { log(Log_level::ERROR, string); }
+    void error(const boost::format & string_fmt) const { error(boost::str(string_fmt)); }
+
+private:
+    static bool exist_instance_flag;
+    static Logger * singleton;
+
+    std::ostream * log_stream;
+
+    int indent_level;
+    Log_level log_level;
+
+    Logger() // Private constructor
+	{
+	    log_stream = &std::cerr;
+	    indent_level = 0;
+	    log_level = Log_level::INFO;
+	}
+};
+
+#endif
