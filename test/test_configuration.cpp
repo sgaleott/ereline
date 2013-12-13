c1a221ca4c3343fc82e00cde294ff461bc369875
Maurizio Tomasi <ziotom78@gmail.com>
2013-12-13 16:41:46 +0100

    Better unit tests for the Logger and the Configuration classes

--- test/test_configuration.cpp
+++ test/test_configuration.cpp
@@ -1,6 +1,36 @@
-#include <iostream>
+#include "common/configuration.hpp"
 
-int main()
+#define BOOST_TEST_MODULE "Configuration"
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(variable_substitution_test)
 {
-    return 0;
+    Configuration conf;
+
+    conf.set_variable("test1", "value1");
+    conf.set_variable("test2", "value2");
+
+    // No substitution at all
+    BOOST_CHECK_EQUAL(conf.substitute_variables("-- test1 is test2 --"),
+		      "-- test1 is test2 --");
+    // Basic variable substitution
+    BOOST_CHECK_EQUAL(conf.substitute_variables("-- {test1} is {test2} --"),
+		      "-- value1 is value2 --");
+    // Check that the substitution works at the beginning of the string
+    BOOST_CHECK_EQUAL(conf.substitute_variables("{test1} --"),
+		      "value1 --");
+    // Check that the substitution works at the end of the string
+    BOOST_CHECK_EQUAL(conf.substitute_variables("{test1} --"),
+		      "value1 --");
+    // Check that multiple substitution work
+    BOOST_CHECK_EQUAL(conf.substitute_variables("{test1}{test1}{test1}"),
+		      "value1value1value1");
+
+    // Check that undefined variables are properly detected
+    BOOST_CHECK_THROW(conf.substitute_variables("{does_not_exist}"),
+		      Configuration_error);
+
+    // Check that unmatched '{}' are detected
+    BOOST_CHECK_THROW(conf.substitute_variables("{test1"),
+		      Configuration_error);
 }
--- test/test_logger.cpp
+++ test/test_logger.cpp
@@ -2,13 +2,40 @@
 
 #include <sstream>
 
-#define BOOST_TEST_MODULE Logging
-#include <boost/test/included/unit_test.hpp>
+#define BOOST_TEST_MODULE "Logging"
+#include <boost/test/unit_test.hpp>
 
 BOOST_AUTO_TEST_CASE(logging_test)
 {
     Logger * log = Logger::get_instance();
-    std::stringstream str;
+    std::stringstream log_stream;
 
-    log->set_stream(&str);
+    log->set_stream(&log_stream);
+    log->set_log_level(Logger::Log_level::INFO);
+
+    const std::string debug_str("This is a debug message");
+    const std::string info_str("This is just some plain information");
+    const std::string warning_str("Warning! Something suspicious has happened");
+    const std::string error_str("Error!");
+
+    log->debug(debug_str); // This should be ignored
+    log->info(info_str);
+    log->warning(warning_str);
+    log->error(error_str);
+
+    log_stream.seekg(0); // Move the stream back to start
+    std::cout << "The string is:\n\"" << log_stream.str() << "\"\n\n";
+    log_stream.seekg(0); // Move the stream back to start
+
+    std::string cur_line;
+
+    // Do not check for the debug message, Logger has ignored it
+    const int length_of_log_header = 29;
+    std::getline(log_stream, cur_line);
+    BOOST_CHECK_EQUAL(info_str, cur_line.substr(length_of_log_header));
+    std::getline(log_stream, cur_line);
+    BOOST_CHECK_EQUAL(warning_str, cur_line.substr(length_of_log_header));
+    std::getline(log_stream, cur_line);
+    BOOST_CHECK_EQUAL(error_str, cur_line.substr(length_of_log_header));
 }
+
