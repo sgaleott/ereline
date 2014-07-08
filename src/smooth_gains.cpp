3b4da6c3ba4bd5a6d5148411897d8b7ee7ed427d
Maurizio Tomasi <ziotom78@gmail.com>
2014-07-08 16:40:27 +0200

    Steps towards a real integration of the modules.
    The Configuration object is now really able to do something useful, i.e.,
    now it initializes a number of variables depending on the JSON file it
    reads. Moreover, small wrappers (currently empty) around the main
    pipeline modules (e.g., DipoleFit, DaCapo...) have been added.

---
+++ src/smooth_gains.cpp
@@ -0,0 +1,7 @@
+#include "smooth_gains.hpp"
+#include "configuration.hpp"
+
+void
+run_smooth_gains(const Configuration & conf)
+{
+}
---
+++ src/smooth_gains.hpp
@@ -0,0 +1,8 @@
+#ifndef SMOOTH_GAINS
+#define SMOOTH_GAINS
+
+class Configuration;
+
+void run_smooth_gains(const Configuration & conf);
+
+#endif
