330671435dd9a7bbe789a29ae4da1addecf7fab9
Andrea Zonca <code@andreazonca.com>
2014-10-13 09:56:29 -0700

    interface to be used from python

---
+++ src/smooth_interface.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "gain_table.hpp"
+#include "smooth_interface.h"
+
+int smoothGains(int npids,
+                int * pid,
+                double * gain,
+                double * dipole,
+                double * outputGain)
+{
+// horn|rad|smooth_window_near_dipole_min|smooth_window_near_dipole_max|smooth_window_length|fast_variations_window_length|slow_var_percentile|fast_variations_percentile|dipole_range_min_value|dipole_range_max_value
+// 19|1|1800|400|150|300|0.995|0.95|0.003|0.0035
+
+    int windowLenMinima = 1800;
+    int windowLenMaxima = 400;
+    int windowLenSlowSmoothing = 150;
+    double percentSlowVariations = 0.995;
+    double minRangeDipole = 3.0e-3;
+    double maxRangeDipole = 3.5e-3;
+    std::vector<double> dipoleVec;
+
+    std::cout << pid[0] << std::endl;
+    std::cout << pid[1] << std::endl;
+    std::cout << gain[0] << std::endl;
+    std::cout << gain[1] << std::endl;
+    std::cout << dipole[0] << std::endl;
+    std::cout << dipole[1] << std::endl;
+
+    Gain_table_t outRawTable;
+    for (int i = 0; i < npids; i++)
+    {
+        outRawTable.append({pid[i], gain[i], 0. });
+        dipoleVec.push_back(dipole[i]);
+    }
+    std::vector<double> outputGainVec = outRawTable.gainSmoothing(windowLenMinima, windowLenMaxima,
+            windowLenSlowSmoothing, percentSlowVariations,
+            minRangeDipole, maxRangeDipole, dipoleVec);
+    for (int i = 0; i < npids; i++)
+    {
+        outputGain[i] = outputGainVec[i];
+    }
+    std::cout << outputGain[0] << std::endl;
+    std::cout << outputGain[1] << std::endl;
+    return 0;
+}
---
+++ src/smooth_interface.h
@@ -0,0 +1,6 @@
+#ifndef SMOOTH_INTERFACE_HPP
+#define SMOOTH_INTERFACE_HPP
+
+int smoothGains(int npids, int * pid, double * gain, double * dipole, double * outputGain);
+
+#endif
